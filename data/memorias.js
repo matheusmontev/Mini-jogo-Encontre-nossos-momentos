372c1ecb44472a1bc2e3ade554fec8212786a233
matheusmontev <ericafilha0@gmail.com>
2026-05-13 19:57:02 -0300

    feat: add victory screen and update memory data with new photo asset

--- data/memorias.js
+++ data/memorias.js
@@ -4,43 +4,43 @@ const MEMORIAS = [
     id: 1,
     foto1: "../fotos/1.1.jpg",
     foto2: "../fotos/1.2.jpg",
-    mensagem: "Nosso primeiro café juntos ☕",
-    data: "12 de Outubro de 2023"
+    mensagem: "nosso primeiro passei em familia",
+    data: "4 de Outubro de 2025"
   },
   {
     id: 2,
     foto1: "../fotos/2.1.jpg",
     foto2: "../fotos/2.2.jpg",
-    mensagem: "Pôr do sol inesquecível",
-    data: "24 de Novembro de 2023"
+    mensagem: "ano novo",
+    data: "31 de Dezembro de 2025"
   },
   {
     id: 3,
     foto1: "../fotos/3.1.jpg",
     foto2: "../fotos/3.2.jpg",
-    mensagem: "Flores de uma tarde de domingo",
-    data: "05 de Dezembro de 2023"
+    mensagem: "Você é a flor mais linda que já vi",
+    data: "06 de Março de 2026"
   },
   {
     id: 4,
     foto1: "../fotos/4.1.jpg",
     foto2: "../fotos/4.2.jpg",
-    mensagem: "Nossa caminhada por Sintra",
-    data: "15 de Janeiro de 2024"
+    mensagem: "Nosso cotidiano",
+    data: "02 de Março de 2026"
   },
   {
     id: 5,
     foto1: "../fotos/5.1.jpg",
     foto2: "../fotos/5.2.jpg",
-    mensagem: "Assistindo as estrelas",
-    data: "14 de Fevereiro de 2024"
+    mensagem: "quero poder jantar todas as noites com você",
+    data: "14 de Abril de 2026"
   },
   {
     id: 6,
     foto1: "../fotos/6.1.jpg",
     foto2: "../fotos/6.2.jpg",
-    mensagem: "Piquenique no parque",
-    data: "20 de Março de 2024"
+    mensagem: "você é minha estrelinha cadente 👎🏻",
+    data: "06 de Março de 2026"
   }
 ];
 
